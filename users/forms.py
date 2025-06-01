553bfe6b11a0b0179fe46f328175baa6b7115f1e
iklobato <iklobato1@gmail.com>
2025-06-01 00:18:20 -0300

    Revamp login and waiting list pages with improved design.
    Updated the login and waiting list success templates to enhance UI/UX. Changes include a cleaner layout, improved color contrast, messaging, and added user interaction features like positioning benefits and social sharing options.

--- users/forms.py
+++ users/forms.py
@@ -8,23 +8,23 @@ class WaitingListForm(forms.ModelForm):
         fields = ['email', 'first_name', 'last_name', 'company', 'use_case']
         widgets = {
             'email': forms.EmailInput(attrs={
-                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
+                'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-white shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50',
                 'placeholder': 'your.email@example.com'
             }),
             'first_name': forms.TextInput(attrs={
-                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
+                'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-white shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50',
                 'placeholder': 'John'
             }),
             'last_name': forms.TextInput(attrs={
-                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
+                'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-white shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50',
                 'placeholder': 'Doe'
             }),
             'company': forms.TextInput(attrs={
-                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
+                'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-white shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50',
                 'placeholder': 'Your Company (optional)'
             }),
             'use_case': forms.Textarea(attrs={
-                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50',
+                'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-white shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50',
                 'rows': 4,
                 'placeholder': 'Tell us about your use case for financial data (optional)'
             }),
